ef3285d02414a7b79ed77854f3d6595cfe45dd85
James Freill <jamesfreill@live.com>
2014-09-16 14:38:48 -0400

    .NET - Lab2
    basic .net application.

---
+++ Strings.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Strings.aspx.cs" Inherits="_Strings" %>
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta charset="utf-8" />
+    <title></title>    
+</head>
+<body>
+    <form id="form1" runat="server">   
+        <div id="input" runat="server">
+            <h2>Input</h2>
+            <asp:TextBox ID="inputTextBox" runat="server" TextMode="MultiLine" Width="450px" Height="75px" />
+            <br />
+            <br />
+            <asp:Button ID="inputButton" runat="server" Text="Transform User Text" OnClick="inputButton_OnClick"/>
+            <br />
+        </div>
+        <div id="output" runat="server">
+            <h2>Output</h2>
+            <asp:Literal ID="outputLiteral" runat="server"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Strings.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+public class ExtensionMethods
+{
+    public string ToUpperLower(string inputString)
+    {
+
+        int i = 0;
+        char[] input = inputString.ToLower().ToCharArray();
+        while (i < input.Length)
+        {
+            input[i] = char.ToUpper(input[i]);
+            i += 2;
+        }
+        return new string(input);
+    }
+}
+/// <summary>
+/// Summary description for _Strings
+/// </summary>
+public partial class _Strings : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void inputButton_OnClick(Object sender, EventArgs e)
+    {
+        this.outputLiteral.Text = this.inputTextBox.Text.ToUpperLower();
+    }
+}
+
